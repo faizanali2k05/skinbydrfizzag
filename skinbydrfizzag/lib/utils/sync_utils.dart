86f3d0b4c840cff220143522212844ad2b249b1f
Faizan Ali <165537437+faizanali2k05@users.noreply.github.com>
2026-03-28 22:47:04 +0500

    Delete skinbydrfizzag directory

--- skinbydrfizzag/lib/utils/sync_utils.dart
+++
@@ -1,8 +0,0 @@
-class SyncUtils {
-  static Future<void> forceCollectionRefresh(String collectionPath) async {}
-
-  static Future<void> forceDocumentRefresh(
-      String collectionPath, String documentId) async {}
-
-  static Future<void> waitForPendingWrites() async {}
-}
